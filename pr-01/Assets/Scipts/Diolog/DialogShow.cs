c6c93960576317b1fc65b1d7e5ed9af1a474c60f
Rustem Maldybekov <mrustem501@gmail.com>
2022-10-31 10:52:01 +0600

    Remake the dialogue system
    - Created the better way of using coroutines
    - Deleted the default inventory panel

--- pr-01/Assets/Scipts/Diolog/DialogShow.cs
+++ pr-01/Assets/Scipts/Diolog/DialogShow.cs
@@ -6,67 +6,42 @@ public class DialogShow : MonoBehaviour
 {
 
     [SerializeField] private GameObject _dialogueWindow;
-    [SerializeField] private GameObject _dialogAnswerPanel;
-    [SerializeField] private GameObject _inventoryPanel;
+    [SerializeField] private GameObject _answersPanel;
     [SerializeField] private TMP_Text _message;
 
-    private Animator _answersAnim;
-    private Animator _inventoryAnim;
-
-    private void Start()
-    {
-        _answersAnim = _dialogAnswerPanel.GetComponent<Animator>();
-        _inventoryAnim = _inventoryPanel.GetComponent<Animator>();
-    }
+    [SerializeField] private Animator _answersAnim;
 
     public void ShowDialogue()
     {
-        _dialogueWindow.SetActive(!_dialogueWindow.activeSelf);
-        StartCoroutine(ShowAnswers());
+        if (!_answersPanel.activeSelf)
+            StartCoroutine(ShowAnswersPanel());
+        else
+            StartCoroutine(HideAnswersPanel());
     }
 
     public void InitMessage(string message)
     {
+        _dialogueWindow.SetActive(!_dialogueWindow.activeSelf);
         _message.text = default;
         StartCoroutine(TypeMessage(message));
     }
 
-    private IEnumerator ShowAnswers()
+    private IEnumerator HideAnswersPanel()
     {
-        string res = default;
-        ShowInteractionPanel(_answersAnim, _dialogAnswerPanel, ref res);
-        ShowInteractionPanel(_inventoryAnim, _inventoryPanel, ref res);
-
-        yield return new WaitForSeconds(GetTheLongerClip());
-
-        Debug.Log(res);
-
-        if (res.Equals(_dialogAnswerPanel.name))
-            _dialogAnswerPanel.SetActive(false);
-        else if (res.Equals(_inventoryPanel.name))
-            _inventoryPanel.SetActive(false);
+        _answersAnim.SetTrigger("on_hide");
+        yield return new WaitForSeconds(_answersAnim.GetCurrentAnimatorStateInfo(0).length);
+        _answersPanel.SetActive(false);
     }
 
-    private void ShowInteractionPanel(Animator anim, GameObject panel, ref string res)
+    private IEnumerator ShowAnswersPanel()
     {
-        if (panel.activeSelf)
-        {
-            anim.SetTrigger("on_hide");
-            res = panel.name;
-        }
-        else
-        {
-            panel.SetActive(true);
-            anim.SetTrigger("on_show");
-        }
+        _answersPanel.SetActive(true);
+        _answersAnim.SetTrigger("on_show");
+        yield return new WaitForSeconds(_answersAnim.GetCurrentAnimatorStateInfo(0).length);
     }
-
-    private float GetTheLongerClip() =>
-        Mathf.Max(_inventoryAnim.GetCurrentAnimatorStateInfo(0).length, _answersAnim.GetCurrentAnimatorStateInfo(0).length);
 
     private IEnumerator TypeMessage(string message)
     {
-
         for (int i = 0; i < message.Length; ++i)
         {
             _message.text += message[i];
